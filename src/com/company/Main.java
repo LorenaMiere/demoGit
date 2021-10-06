58603de3ae43b0c3444e302229246f95739698f3
catabava <catalinrus03@gmail.com>
2021-10-06 18:10:38 +0300

    third commit cata, master branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,4 +13,8 @@ public class Main {
     public static void cata2(){
         System.out.println("second commit cata"); //still on master branch
     }
+
+    public static void catalin3(){
+        System.out.println("third commit cata"); //still on master branch
+    }
 }
