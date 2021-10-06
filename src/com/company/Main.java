e5d709f949392c7cb1b79bb318ee8f13d1e18255
lore <loremiere@gmail.com>
2021-10-06 17:48:01 +0300

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("first commit");
-
-        System.out.println("first commit Cata -> master");
+        System.out.println("first commit Lore - test branch");
+        System.out.println("second commit Lore - test branch");
     }
 }
