f5540dc7cc1d9a1fed01988d90bfd63bf60d1177
lore <loremiere@gmail.com>
2021-10-07 09:12:04 +0300

    new feature

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("first commit");
     }
+
+    public static void testBranchFeature() {
+        System.out.println("new feature");
+    }
 }
